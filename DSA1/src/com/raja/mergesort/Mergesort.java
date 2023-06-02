3def433dbf4884e3ea96b44e7ac38e95ba243838
rajapedd <rajapeddanna.kadavakallu@gmail.com>
2023-06-02 12:23:37 +0530

    Merge Sort v2

--- DSA1/src/com/raja/mergesort/Mergesort.java
+++ DSA1/src/com/raja/mergesort/Mergesort.java
@@ -2,7 +2,9 @@ package com.raja.mergesort;
 
 public class Mergesort {
     public static void main(String[] args) {
-        System.out.println("Practice Merge Sort");
+        System.out.println("Practice Merge Sort1");
+
+        System.out.println("Practice Merge Sort2");
 
     }
 }
